812dcbf5cb8a4ee5a1e7cfb228e8fe4805ce0079
Michael Martins <michaelfm21@gmail.com>
2021-04-08 10:32:48 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/dropzone.css">
+    <script src="dropzone.min.js"></script>
+    <title>Dropzone.js</title>
+</head>
+<body>
+    <form action="upload.php" class="dropzone"></form>
+</body>
+</html>
